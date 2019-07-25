7ada8005914ebdfd3807d896ed3df9af358ab63d
Scott Boggs <scott@tams.tech>
2019-07-25 17:55:20 +0000

    The project is now feature complete.

--- test-delegate.py
+++ test-delegate.py
@@ -1,4 +1,5 @@
 from delegate import delegate
+from contextlib import contextmanager
 class Parent:
     def __init__(self):
         self.a = "a"
@@ -6,12 +7,23 @@ class Parent:
 
 @delegate("a", "b", to="parent")
 class Child:
+    """A class with a delegate"""
     def __init__(self):
         self.parent = Parent()
         self.c = "c"
 
 
 print("TESTING delegate()...", end='\r')
+
+@contextmanager
+def expect_raises(errtype):
+    err = None
+    try:
+        yield
+    except errtype as e:
+        err = e
+    finally:
+        assert err
 
 instance = Child()
 assert instance.a == instance.parent.a == "a"
@@ -20,13 +32,14 @@ print(instance.__dict__, instance.parent.__dict__)
 assert instance.b == instance.parent.b == "d"
 err = None
 del instance.a
-try: instance.a
-except Exception as e: err = e
-finally: assert err
+with expect_raises(Exception):
+    instance.a
 assert instance.c == 'c'
-err = None
-try: instance.parent.c
-except Exception as e: err = e
-finally: assert err
-z
+with expect_raises(Exception):
+    instance.parent.c
+assert instance.__doc__ == "A class with a delegate"
+assert instance.__class__.__name__ == "Child"
+with expect_raises(AttributeError):
+    instance.z
+
 print("TESTING delegate()...done")
